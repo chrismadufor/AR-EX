ade8592f17104b05a82c62ab05f54a9ef576ccfe
Christian Madufor <chrismadufor@gmail.com>
2022-04-03 20:18:13 +0100

    Add files via upload

---
+++ js/app-makers.js
@@ -0,0 +1,41 @@
+// Get array of objects of the sponsors
+
+let sponsors = [
+    {
+        name: 'paystack',
+        url: './assets/markers/paystack',
+        modelUrl: './assets/models/paystack/scene.gltf',
+        message: 'Hi, Paystack is amazing!'
+    },
+    {
+        name: 'piggyvest',
+        url: './assets/markers/piggyvest',
+        modelUrl: './assets/models/piggyvest/scene.gltf',
+        message: 'Hi, hit your savings targets with Piggyvest'
+    }
+]
+
+AFrame.registerComponent('markers_start', {
+    init:function() {
+        let sceneEl = document.querySelector('a-scene');
+
+        sponsors.forEach((sponsor, index) => {
+            //create asset
+
+            //create marker
+            let markerEl = document.createElement('a-marker');
+            markerEl.setAttribute('type','pattern');
+			markerEl.setAttribute('url', sponsor.url);
+            markerEl.setAttribute('id', sponsor.name);
+            markerEl.setAttribute('markerEvents', '');
+            //create entity and add as child to marker
+            let model = document.createElement('a-entity')
+            model.setAttribute('id',sponsor.name);
+            model.setAttribute('gltf-model', sponsor.modelUrl);
+
+            markerEl.appendChild(model)
+            sceneEl.appendChild(markerEl)
+        })
+    }
+})
+
