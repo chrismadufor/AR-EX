de8009fbaa34687956ae0503233f1fd4568e8643
chrismadufor <chrismadufor@gmail.com>
2022-04-03 22:05:37 +0100

    image

--- js/app-markers.js
+++ js/app-markers.js
@@ -11,14 +11,14 @@ function loadSponsors() {
     return [
     {
         name: 'paystack',
-        url: 'assets/markers/paystack.png',
-        modelUrl: 'assets/models/paystack/scene.gltf',
+        url: '../assets/markers/paystack.png',
+        modelUrl: '../assets/models/paystack/scene.gltf',
         message: 'Hi, Paystack is amazing!'
     },
     {
         name: 'piggyvest',
-        url: './assets/markers/piggyvest.png',
-        modelUrl: './assets/models/piggyvest/scene.gltf',
+        url: '../assets/markers/piggyvest.png',
+        modelUrl: '../assets/models/piggyvest/scene.gltf',
         message: 'Hi, hit your savings targets with Piggyvest'
     },
 ]}
