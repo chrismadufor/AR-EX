c1f3b24d3ebf4a6e117d0439a503981856f09a09
chrismadufor <chrismadufor@gmail.com>
2022-04-03 21:11:35 +0100

    rearranged js file

--- js/app-markers.js
+++ js/app-markers.js
@@ -1,8 +1,14 @@
 // Get array of objects of the sponsors
 
+window.onload = () => {
+    let sponsors = loadSponsors();
+    renderMarkers(sponsors);
+};
+
 console.log('File Loaded')
 
-let sponsors = [
+function loadSponsors() {
+    return [
     {
         name: 'paystack',
         url: './assets/markers/paystack',
@@ -14,31 +20,29 @@ let sponsors = [
         url: './assets/markers/piggyvest',
         modelUrl: './assets/models/piggyvest/scene.gltf',
         message: 'Hi, hit your savings targets with Piggyvest'
-    }
-]
+    },
+]}
 
-AFrame.registerComponent('markers_start', {
-    init:function() {
-	    console.log('Init function loaded')
-        let sceneEl = document.querySelector('a-scene');
+function renderMarkers(sponsors) {
+    console.log('Init function loaded')
+    let sceneEl = document.querySelector('a-scene');
 
-        sponsors.forEach((sponsor, index) => {
-            //create asset
+    sponsors.forEach((sponsor, index) => {
+        //create asset
 
-            //create marker
-            let markerEl = document.createElement('a-marker');
-            markerEl.setAttribute('type','pattern');
-			markerEl.setAttribute('url', sponsor.url);
-            markerEl.setAttribute('id', sponsor.name);
-            markerEl.setAttribute('markerEvents', '');
-            //create entity and add as child to marker
-            let model = document.createElement('a-entity')
-            model.setAttribute('id',sponsor.name);
-            model.setAttribute('gltf-model', sponsor.modelUrl);
+        //create marker
+        let markerEl = document.createElement('a-marker');
+        markerEl.setAttribute('type','pattern');
+        markerEl.setAttribute('url', sponsor.url);
+        markerEl.setAttribute('id', sponsor.name);
+        markerEl.setAttribute('markerEvents', '');
+        //create entity and add as child to marker
+        let model = document.createElement('a-entity')
+        model.setAttribute('id',sponsor.name);
+        model.setAttribute('gltf-model', sponsor.modelUrl);
 
-            markerEl.appendChild(model)
-            sceneEl.appendChild(markerEl)
-        })
-    }
-})
+        markerEl.appendChild(model)
+        sceneEl.appendChild(markerEl)
+    })
+}
 
