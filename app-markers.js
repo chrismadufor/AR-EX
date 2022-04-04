727bb0f712a4c2bc1364d1d81286c84d4ba46f93
chrismadufor <chrismadufor@gmail.com>
2022-04-04 09:38:07 +0100

    image

--- app-markers.js
+++ app-markers.js
@@ -23,41 +23,6 @@ function loadSponsors() {
     },
 ]}
 
-function renderMarkers(sponsors) {
-    console.log('Init function loaded')
-    let sceneEl = document.querySelector('a-scene');
-
-    sponsors.forEach((sponsor, index) => {
-        //create asset
-        let assetEl = document.createElement('a-assets')
-        let assetItemEl = document.createElement('a-asset-item')
-        assetItemEl.setAttribute('id', `${sponsor.name}-asset`)
-        assetItemEl.setAttribute('src', sponsor.modelUrl)
-        assetEl.appendChild(assetItemEl)
-
-//         <a-assets>
-//     <a-asset-item id="cityModel" src="https://cdn.aframe.io/test-models/models/glTF-2.0/virtualcity/VC.gltf"></a-asset-item>
-//   </a-assets>
-
-        //create marker
-        let markerEl = document.createElement('a-marker');
-        markerEl.setAttribute('type','pattern');
-        markerEl.setAttribute('url', sponsor.url);
-        markerEl.setAttribute('id', `${sponsor.name}-marker`);
-        markerEl.setAttribute('type', 'pattern');
-        markerEl.setAttribute('preset', 'custom');
-        //create entity and add as child to marker
-        let model = `<a-entity
-        gltf-model="#${sponsor.name}-asset"
-        scale="2">
-    </a-entity>`
-    console.log('Backyard works!!')
-        markerEl.innerHTML = model
-        sceneEl.appendChild(assetEl)
-        sceneEl.appendChild(markerEl)
-    })
-}
-
 //improvements
 // 1. get a better model
 // 2. make the object rotate, if possible
@@ -66,9 +31,36 @@ function renderMarkers(sponsors) {
 // 1. marker-found/object-revealed: Show clickable button to claim prize.
 // 2. click: open modal that gives feedback.
 
-// AFRAME.registerComponent('markers_start',{
-// 	init:function(){
-//         console.log('component thing is working')
-//     }
-// })
+AFRAME.registerComponent('markers_start',{
+	init:function(sponsors){
+        console.log('Init function loaded')
+        let sceneEl = document.querySelector('a-scene');
+    
+        sponsors.forEach((sponsor, index) => {
+            //create asset
+            let assetEl = document.createElement('a-assets')
+            let assetItemEl = document.createElement('a-asset-item')
+            assetItemEl.setAttribute('id', `${sponsor.name}-asset`)
+            assetItemEl.setAttribute('src', sponsor.modelUrl)
+            assetEl.appendChild(assetItemEl)
+    
+            //create marker
+            let markerEl = document.createElement('a-marker');
+            markerEl.setAttribute('type','pattern');
+            markerEl.setAttribute('url', sponsor.url);
+            markerEl.setAttribute('id', `${sponsor.name}-marker`);
+            markerEl.setAttribute('type', 'pattern');
+            markerEl.setAttribute('preset', 'custom');
+            //create entity and add as child to marker
+            let model = `<a-entity
+            gltf-model="#${sponsor.name}-asset"
+            scale="2">
+        </a-entity>`
+        console.log('Backyard works!!')
+            markerEl.innerHTML = model
+            sceneEl.appendChild(assetEl)
+            sceneEl.appendChild(markerEl)
+        })
+    }
+})
 
