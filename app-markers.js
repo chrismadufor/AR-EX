c9da77422ed2950e0440d0742e2c1fc7b6ae8ae3
chrismadufor <chrismadufor@gmail.com>
2022-04-04 09:48:30 +0100

    image

--- app-markers.js
+++ app-markers.js
@@ -1,9 +1,4 @@
 // Get array of objects of the sponsors
-let sponsors;
-window.onload = () => {
-    sponsors = loadSponsors();
-};
-
 console.log('File Loaded')
 
 function loadSponsors() {
@@ -32,6 +27,21 @@ function loadSponsors() {
 
 AFRAME.registerComponent('markers_start',{
 	init:function(){
+        let sponsors = [
+            {
+                name: 'paystack',
+                url: 'assets/markers/paystack-marker.patt',
+                modelUrl: 'assets/models/paystack/scene.gltf',
+                message: 'Hi, Paystack is amazing!'
+            },
+            {
+                name: 'piggyvest',
+                url: 'assets/markers/piggyvest-marker.patt',
+                modelUrl: 'assets/models/piggyvest/scene.gltf',
+                message: 'Hi, hit your savings targets with Piggyvest'
+            },
+        ]
+        
         console.log('Init function loaded')
         let sceneEl = document.querySelector('a-scene');
     
