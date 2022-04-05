48f013cb5519b9b359cb7c27496902a032052e57
chrismadufor <chrismadufor@gmail.com>
2022-04-05 05:26:19 +0100

    js

--- app-markers.js
+++ app-markers.js
@@ -84,19 +84,19 @@ AFRAME.registerComponent('markers_start',{
     }
 })
 
-AFRAME.registerComponent('markerevents', {
-    init: function() {
+// AFRAME.registerComponent('markerevents', {
+//     init: function() {
         
 
-        // every click, we make our model grow in size :)
-        // animatedMarker.addEventListener('click', function(ev, target){
-        //     const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-        //     if (aEntity && intersectedElement === aEntity) {
-        //         const scale = aEntity.getAttribute('scale');
-        //         Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-        //         aEntity.setAttribute('scale', scale);
-        //     }
-        // });
-    }
-});
+//         // every click, we make our model grow in size :)
+//         // animatedMarker.addEventListener('click', function(ev, target){
+//         //     const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+//         //     if (aEntity && intersectedElement === aEntity) {
+//         //         const scale = aEntity.getAttribute('scale');
+//         //         Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+//         //         aEntity.setAttribute('scale', scale);
+//         //     }
+//         // });
+//     }
+// });
 
